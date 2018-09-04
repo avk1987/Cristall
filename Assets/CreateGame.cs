58cca900efe1dc1b2422de523953038d8ab6cf0d
avk1987 <39376921+avk1987@users.noreply.github.com>
2018-09-04 19:14:58 +0500

    Add files

---
+++ Assets/CreateGame.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGame : MonoBehaviour {
+
+    public GameObject firtst;
+    public GameObject second;
+
+    public GameObject prefab;
+    private float y =  0.5f;
+
+
+    void Start () {
+
+        while(y < 19) {
+           float x = -12.5f;
+
+            while (x < 13.5f)
+            {
+               GameObject go = Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);  
+               GerRndColor(go, x, y);
+                x++;
+             }
+             y++;
+           
+        }
+    }
+	
+    void GerRndColor(GameObject go, float x, float y)
+    {
+        SpriteRenderer SprRen = go.GetComponent<SpriteRenderer>();
+        int rnd = (int)Random.Range(1, 5);
+
+
+        switch (rnd)
+        {
+            case 1:
+                SprRen.color = Color.red;
+                break;
+            case 2:
+                SprRen.color = Color.green;
+                break;
+            case 3:
+                SprRen.color = Color.blue;
+                break;
+            case 4:
+                SprRen.color = Color.yellow;
+                break;
+        }
+
+        go.GetComponent<fall>().color = rnd.ToString();
+        go.name = x.ToString() + "|" + y.ToString();
+
+    }
+}
---
+++ Assets/fall.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fall : MonoBehaviour
+{
+    public string color;
+
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 nw = transform.position;
+        nw.y = nw.y - 0.5f;
+        RaycastHit2D hit = Physics2D.Raycast(nw, Vector2.down);
+        Vector2 dest = hit.transform.position;
+        dest.y = dest.y + 1;
+        dest.x = transform.position.x;
+        transform.position = Vector2.MoveTowards(transform.position, dest, Time.deltaTime * 20);
+    }
+
+    private void OnMouseDown()
+    {
+        CreateGame obj = Camera.main.GetComponent<CreateGame>();
+        
+        if (obj.firtst ==null)
+        {
+            obj.firtst = this.gameObject;
+        } else
+        {
+            obj.second = this.gameObject;
+            if(isnear(obj.firtst, obj.second))
+            {
+                //сделать ход
+            }
+        }
+
+    }
+
+    private bool isnear(GameObject first, GameObject second)
+    {
+
+        int fx = (int)first.transform.position.x;
+        int sx = (int)second.transform.position.x;
+        int fy = (int)first.transform.position.y;
+        int sy = (int)second.transform.position.y;
+
+        if (((fx-sx == 1)|| (fx-sx == -1)) & (fy == sy)) // проверка по горизонтали
+        {
+            return true;   // значит по иксу они рядом
+        }
+
+        return false;
+    }
+}
+
+//Destroy(gameObject);
